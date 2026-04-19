659e7144d05ea7c5bd9d4c8934aa8671238b01e7
aurelien-aubry <35953576+Harzak@users.noreply.github.com>
2026-04-19 20:40:11 +0200

    simplify ViewModelFactory with IServiceProvider

--- deavnote.app/ViewModels/Base/ViewModelFactory.cs
+++ deavnote.app/ViewModels/Base/ViewModelFactory.cs
@@ -1,96 +1,55 @@
 ﻿using deavnote.app.ViewModels.Search;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace deavnote.app.ViewModels.Base;
 
 /// <summary>
-/// Provides methods to create view model instances for journals and time entries.
+/// Provides methods to create view model instances using the service provider for dependency resolution.
 /// </summary>
 internal sealed class ViewModelFactory : IViewModelFactory
 {
-    private readonly IJournal _journal;
-    private readonly IDevTaskRepository _taskRepository;
-    private readonly ISearchRepository _searchRepository;
-    private readonly ITimeEntryRepository _timeEntryRepository;
-    private readonly IDateProvider _dateProvider;
-    private readonly IDialogService _dialogService;
-    private readonly Lazy<IViewOrchestrator> _viewOrchestrator;
-    private readonly INotificationService _notificationService;
-    private readonly IClipboardService _clipboardService;
+    private readonly IServiceProvider _serviceProvider;
 
-    public ViewModelFactory(
-        IJournal journal,
-        IDevTaskRepository taskRepository,
-        ISearchRepository searchRepository,
-        ITimeEntryRepository timeEntryRepository,
-        IDateProvider dateProvider,
-        IDialogService dialogService,
-        Lazy<IViewOrchestrator> viewOrchestrator,
-        INotificationService notificationService,
-        IClipboardService clipboardService)
+    public ViewModelFactory(IServiceProvider serviceProvider)
     {
-        ArgumentNullException.ThrowIfNull(journal);
-        ArgumentNullException.ThrowIfNull(taskRepository);
-        ArgumentNullException.ThrowIfNull(searchRepository);
-        ArgumentNullException.ThrowIfNull(timeEntryRepository);
-        ArgumentNullException.ThrowIfNull(dateProvider);
-        ArgumentNullException.ThrowIfNull(dialogService);
-        ArgumentNullException.ThrowIfNull(viewOrchestrator);
-        ArgumentNullException.ThrowIfNull(notificationService);
-        ArgumentNullException.ThrowIfNull(clipboardService);
-
-        _journal = journal;
-        _taskRepository = taskRepository;
-        _searchRepository = searchRepository;
-        _timeEntryRepository = timeEntryRepository;
-        _dateProvider = dateProvider;
-        _dialogService = dialogService;
-        _viewOrchestrator = viewOrchestrator;
-        _notificationService = notificationService;
-        _clipboardService = clipboardService;
+        ArgumentNullException.ThrowIfNull(serviceProvider);
+        _serviceProvider = serviceProvider;
     }
 
     /// <inheritdoc/>
     public JournalViewModel CreateJournalViewModel()
     {
-        return new JournalViewModel(
-            _journal,
-            _dateProvider,
-            viewModelFactory: this,
-            _dialogService,
-            _viewOrchestrator.Value,
-            _notificationService,
-            _clipboardService);
+        return ActivatorUtilities.CreateInstance<JournalViewModel>(_serviceProvider);
     }
 
     /// <inheritdoc/>
     public TimeEntryListItemViewModel CreateTimeEntryViewModel(model.Entities.TimeEntry timeEntry)
     {
         ArgumentNullException.ThrowIfNull(timeEntry);
-
-        return new TimeEntryListItemViewModel(timeEntry, _clipboardService, _notificationService);
+        return ActivatorUtilities.CreateInstance<TimeEntryListItemViewModel>(_serviceProvider, timeEntry);
     }
 
     /// <inheritdoc/>
     public AddTimeEntryViewModel CreateAddTimeEntryViewModel()
     {
-        return new AddTimeEntryViewModel(_taskRepository);
+        return ActivatorUtilities.CreateInstance<AddTimeEntryViewModel>(_serviceProvider);
     }
 
     /// <inheritdoc/>
     public SearchViewModel CreateSearchViewModel()
     {
-        return new SearchViewModel(_searchRepository, _viewOrchestrator.Value);
+        return ActivatorUtilities.CreateInstance<SearchViewModel>(_serviceProvider);
     }
 
     /// <inheritdoc/>
     public DevTaskDetailViewModel CreateDevTaskDetailViewModel(int id)
     {
-        return new DevTaskDetailViewModel(id, _taskRepository, _notificationService);
+        return ActivatorUtilities.CreateInstance<DevTaskDetailViewModel>(_serviceProvider, id);
     }
 
     /// <inheritdoc/>
     public TimeEntryDetailViewModel CreateTimeEntryDetailViewModel(int id)
     {
-        return new TimeEntryDetailViewModel(id, _timeEntryRepository, factory: this, _notificationService);
+        return ActivatorUtilities.CreateInstance<TimeEntryDetailViewModel>(_serviceProvider, id);
     }
 }
