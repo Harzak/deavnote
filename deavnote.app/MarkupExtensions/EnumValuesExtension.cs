23e65c162c6f70f31873d0a7ed067e2918469937
aurelien-aubry <35953576+Harzak@users.noreply.github.com>
2026-07-17 06:41:40 +0200

    Add DevTask State to UI

---
+++ deavnote.app/MarkupExtensions/EnumValuesExtension.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Markup.Xaml;
+
+namespace deavnote.app.MarkupExtensions;
+
+internal sealed class EnumValuesExtension : MarkupExtension
+{
+    private readonly Type _enumType;
+
+    public EnumValuesExtension(Type enumType)
+    {
+        _enumType = enumType;
+    }
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        return Enum.GetValues(_enumType);
+    }
+}
