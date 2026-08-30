1a9916b8576a329a5950127511f089a5bdae5bc7
aurelien-aubry <35953576+Harzak@users.noreply.github.com>
2026-08-30 18:07:02 +0200

    Default log config + config file
    https://github.com/Harzak/deavnote/issues/32

--- deavnote.app/App.axaml.cs
+++ deavnote.app/App.axaml.cs
@@ -1,7 +1,6 @@
 using Avalonia.Markup.Xaml;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using NLog.Targets;
 
 namespace deavnote.app;
 
@@ -42,7 +41,6 @@ internal sealed partial class App : Application, IDisposable
     {
         string connectionString = DatabasePathResolver.Resolve();
 
-        ConfigureNLog();
         services.AddLogging(builder =>
         {
             builder.ClearProviders();
@@ -60,27 +58,6 @@ internal sealed partial class App : Application, IDisposable
     private void OnShutdownRequested(object? sender, ShutdownRequestedEventArgs e)
     {
         this.Dispose();
-    }
-
-    private static void ConfigureNLog()
-    {
-        string appDataFolder = ApplicationEnvironment.ResolveAppDataFolder();
-
-        NLog.Config.LoggingConfiguration config = new();
-
-#pragma warning disable CA2000 // FileTarget lifecycle is managed by NLog's LogManager.Configuration
-        FileTarget fileTarget = new("file")
-        {
-            FileName = Path.Combine(appDataFolder, "deavnote.${shortdate}.log"),
-            MaxArchiveFiles = 30,
-            Layout = "${longdate}|${level:uppercase=true}|${logger}|${message}${onexception:inner=|${exception:format=tostring}}",
-        };
-#pragma warning restore CA2000
-
-        config.AddTarget(fileTarget);
-        config.AddRule(NLog.LogLevel.Info, NLog.LogLevel.Fatal, fileTarget);
-
-        LogManager.Configuration = config;
     }
 
     public void Dispose()
